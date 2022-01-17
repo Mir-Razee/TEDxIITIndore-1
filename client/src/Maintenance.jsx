397932b1188260043733f5fa6f9c0b958c15cdcd
Suman <sumanj631@gmai.com>
2022-01-17 17:11:20 +0530

    .

---
+++ client/src/Maintenance.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Maintenance() {
+    return (
+        <section className="container">
+            <div className="content">
+                <div className="logo-container text-center">
+                    <img className='mt-5' src="https://res.cloudinary.com/tedxiiti/image/upload/w_300,c_fill/v1642099561/images/logos/removed_white_bg.png" alt="" />
+                </div>
+                <div className="message">
+                    <h1>Coming Soon...!</h1>
+                    <p>on 18th January 2022, 4 PM 👌</p>
+                </div>
+            </div>
+        </section>
+    )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Maintenance from './Maintenance';
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <Maintenance />
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
